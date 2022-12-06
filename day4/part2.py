df235f4312c0e0983d87300e1417f298039d25cd
rbagan <ruben.bagan@gmail.com>
2022-12-06 20:04:28 +0100

    day 4 part 2 is done

---
+++ day4/part2.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def convert_id_range_to_range(id_range: str) -> List[int]:
+    # Input type: X-Y and returns a range from [x, Y]
+    x, y = id_range.split("-")
+    return list(range(int(x), int(y) + 1))
+
+
+with open("data.txt", "r", newline="\n") as input_data:
+
+    overlapping_schedules = 0
+    for entry in input_data.readlines():
+        entry = entry.strip()
+        first_turn, second_turn = entry.split(",")
+
+        first_turn = convert_id_range_to_range(first_turn)
+        second_turn = convert_id_range_to_range(second_turn)
+
+        if len(list(set(first_turn).intersection(set(second_turn)))) > 0:
+            overlapping_schedules += 1
+
+    print(overlapping_schedules)
